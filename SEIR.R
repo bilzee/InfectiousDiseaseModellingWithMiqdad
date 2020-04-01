596ba4d9548bcb0369bfee17c4fa458a36dcda84
Miqdad Asaria <miqdad.asaria@gmail.com>
2020-04-01 21:44:38 +0100

    Very basic implementation of SEIR model and associated shiny app

---
+++ SEIR.R
@@ -0,0 +1,98 @@
+############################################################################################
+# Simple SEIR model implementation
+#
+# Miqdad Asaria
+# April 2020
+############################################################################################
+
+library("tidyverse")
+
+############################################################################################
+# Function to simulate simple SEIR model
+#
+# N = population
+# R0 = basic reproductive number
+# D_pre = average duration of pre-infectious period (days)
+# D = average duration of infectious period (days)
+# delta_t = time step of model (days)
+# times = how many time steps to run the model for
+############################################################################################
+simulate_SEIR = function(N = 100000, R0 = 2.2, D = 2.9, D_pre = 5.2, delta_t = 0.1, days = 100) {
+  # start by using inputs to calculate key model prameters
+  f = delta_t / D_pre
+  r = delta_t / D
+  beta = (R0 * r) / N 
+  iterations = round(days/delta_t)
+  
+  # set up a markov trace to capture the number of people in each state (S,E,I,R) over time
+  markov_trace = matrix(data = NA, nrow = iterations, ncol = 6, dimnames = list(NULL,c("time","S","E","I","R","Total_I")))
+  # intial population in each state at time 0
+  markov_trace[1,"time"] = 0
+  markov_trace[1,"S"] = N-1
+  markov_trace[1,"E"] = 1
+  markov_trace[1,"I"] = 0
+  markov_trace[1,"R"] = 0
+  markov_trace[1,"Total_I"] = 0
+  
+  # run simulation for the desired number of iterations saving results into the markov trace
+  for (t in 2:iterations) {
+    # populations in each state in previous iteration
+    time = markov_trace[t-1, "time"]
+    S = markov_trace[t-1, "S"]
+    E = markov_trace[t-1, "E"]
+    I = markov_trace[t-1, "I"]
+    R = markov_trace[t-1, "R"]
+    Total_I = markov_trace[t-1, "Total_I"]
+      
+    # apply difference equations formulas to calculate new populations in each state
+    markov_trace[t, "time"] = time + delta_t
+    markov_trace[t, "S"] = S - (beta * I * S)
+    markov_trace[t, "E"] = E + (beta * I * S) - (f * E)   
+    markov_trace[t, "I"] = I + (f * E) - (r * I) 
+    markov_trace[t, "R"] = R + (r * I) 
+    markov_trace[t, "Total_I"] = Total_I + (f * E)
+  }
+  
+  # convert matrix to tibble to make it easier to manipulate
+  markov_trace = as_tibble(markov_trace)
+  
+  # calculate the herd immunity threshold
+  HIT = (R0-1)/R0
+  
+  # look at markov trace to see when herd immunity achieved if at all
+  if(markov_trace %>% filter(R>=HIT*N) %>% select(time) %>% nrow() > 0){
+    HIT_date = markov_trace %>% filter(R>=HIT*N) %>% select(time) %>% min() %>% round()
+  } else {
+    HIT_date = NA
+  }
+  
+  # reshape the data into long form for plotting
+  graph_data = markov_trace %>%
+    gather("state", "population", c("S","E","I","R")) %>%
+    mutate(state = factor(state, 
+                          c("S","E","I","R"), 
+                          c("S = susceptible","E = pre-infections","I = infectious","R = recovered")))
+  
+  # plot the SEIR graph
+  seir_plot = ggplot(graph_data, aes(x=time, y=population, group=state)) +
+    xlab("Time (days since first case)") +
+    ylab("Number of people") +
+    geom_line(aes(colour=state, linetype=state)) +
+    labs(title = "Simple infectious disease model",
+         subtitle = bquote("Input parameters:" ~ R[0] ~ " = " ~ .(R0) ~ ", D' = " ~ .(D_pre) ~ ", D = " ~ .(D) ~ ", " ~ delta*t ~ " = " ~ .(delta_t) ~ ", N = " ~ .(N) ~ " Derived parameters: " ~ beta ~ " = " ~ .(beta) ~ ", f = " ~ .(f) ~ ", r = " ~ .(r)),
+         caption = paste0("Herd immunity threshold of ", round(100*HIT) ,"%",ifelse(is.na(HIT_date)," not achieved in this simulation", paste0(" achieved after ",HIT_date," days")))) +
+    scale_colour_manual(name="States", values=c("red","orange","blue","green")) +
+    scale_linetype_manual(name="States", values=c(1,2,1,1)) +
+    theme_bw() + 
+    theme(panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(), 
+          plot.margin = unit(c(1, 1, 1, 1), "lines"),
+          legend.position = "bottom")
+  
+  # return the entire markov trace the SIR plot and the HIT details as outputs of the function
+  return(list("seir_markov_trace"=markov_trace,
+              "seir_plot"=seir_plot,
+              "HIT"=HIT,
+              "HIT_date"=HIT_date))
+}
+
---
+++ app.R
@@ -0,0 +1,103 @@
+############################################################################################
+# Shiny app for simple SEIR model implementation
+#
+# Miqdad Asaria
+# April 2020
+############################################################################################
+
+library(shiny)
+library(DT)
+
+source("SEIR.R")
+
+ui = fluidPage(
+    titlePanel("Simple SEIR model"),
+
+    sidebarLayout(
+        sidebarPanel(
+            numericInput("N",
+                         "Population (N):",
+                         value = 100000, 
+                         min = 1, 
+                         max = NA, 
+                         step = 1),
+            numericInput("R0",
+                         "Basic reproductive number (R0):",
+                         min = 0,
+                         max = 20,
+                         value = 2.2, 
+                         step = 0.1),
+            numericInput("D_pre",
+                        "Average duratiuon of pre-infectious period (D'):",
+                        min = 0,
+                        max = 100,
+                        value = 5.2,
+                        step = 0.1),
+            numericInput("D",
+                        "Average duratiuon of infectious period (D):",
+                        min = 0,
+                        max = 100,
+                        value = 2.9,
+                        step = 0.1),
+            numericInput("delta_t",
+                        "Time step in the model (dt):",
+                        min = 0,
+                        max = 5,
+                        value = 1,
+                        step = 0.1),
+            numericInput("days",
+                        "Number of days to run model for:",
+                        min = 10,
+                        max = 1000,
+                        value = 100,
+                        step = 10)
+        ),
+
+        mainPanel(
+            tabsetPanel(id="tabset",
+                        tabPanel("SEIR Plot", plotOutput("seir_plot"), 
+                                 htmlOutput("summary")),
+                        tabPanel("Markov Trace", div(dataTableOutput("seir_markov_trace"), style = "font-size:70%"))
+            )
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server = function(input, output) {
+    seirModel = reactive({
+        simulate_SEIR(input$N, 
+                     input$R0, 
+                     input$D, 
+                     input$D_pre, 
+                     input$delta_t, 
+                     input$days)
+    })
+    
+    output$seir_plot = renderPlot({
+        seirModel()$seir_plot
+    })
+    
+    output$seir_markov_trace = renderDataTable({
+        datatable(seirModel()$seir_markov_trace,
+                  style = 'bootstrap',
+                  rownames = FALSE,
+                  options = list(pageLength = 20, autoWidth = TRUE, dom='ftrpi'))
+    })
+    
+    output$summary = renderText({
+        data=tail(seirModel()$seir_markov_trace,1)
+        HIT = seirModel()$HIT
+        HIT_date = seirModel()$HIT_date
+        paste0("After ", round(data$time), " days:<br>",
+               "<font color='blue'><b>Infected overall: </b></font> ", round(data$Total_I)," (<b>",round(100*data$Total_I/input$N),"%</b>)<br>", 
+               "<font color='green'><b>Recovered overall: </b></font> ", round(data$R), " (<b>",round(100*data$R/input$N),"%</b>)<br>", 
+               "<font color='red'><b>Susceptible at end: </b></font> ", round(data$S), " (<b>",round(100*data$S/input$N),"%</b>)<br>",
+               "The <b>herd immunity threshold</b> is: <b>", round(100*HIT),"%</b>", 
+               if(is.na(HIT_date)){" not achieved in this simulation"} else {paste0(" achieved after <b>", HIT_date, "</b> days after which number of new infections fall")}
+        )
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
